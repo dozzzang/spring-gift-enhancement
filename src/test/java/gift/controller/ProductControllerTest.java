679c3095d1f2a6561fa18cc8748f92eae1537108
Dohyun <sks020k@naver.com>
2025-07-22 23:37:09 +0900

    test : entity,repository 단위테스트 및 Product통합테스트 구현

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -14,6 +14,7 @@ import gift.product.dto.ProductResponseDto;
 import gift.product.service.ProductService;
 import gift.security.AdminInterceptor;
 import gift.user.service.UserService;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -54,7 +55,8 @@ public class ProductControllerTest {
         "평범한상품",
         10000,
         "https://thisisurl.com",
-        false
+        false,
+        List.of()
     );
 
     when(productService.saveProduct(any(ProductRequestDto.class)))
